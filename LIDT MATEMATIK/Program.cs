8519b5e778a72d3533fdf1786ee7df746eead889
Oliver Tobias Jensen <Olivertobias@live.dk>
2021-01-25 11:57:00 +0100

    Add project files.

---
+++ LIDT MATEMATIK/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIDT_MATEMATIK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Intast det første tal");
+            double tal1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Intast det andet tal");
+            double tal2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            Console.WriteLine(tal1 + "+" + tal2 + " = " + (tal1 + tal2));
+            Console.WriteLine();
+            Console.WriteLine(tal1 + "/" + tal2 + " = " + (tal1 / tal2));
+            Console.WriteLine();
+            Console.WriteLine(tal1 + "^" + tal2 + " = " + Math.Pow(tal1, tal2));
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
